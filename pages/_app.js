e8607fbd6fddfa27b02bd01263e62fd4bd7d9e72
MDaud32 <theprofessor543@gmail.com>
2022-01-16 15:17:51 +0500

    adding chakraui and dummy data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
-
+import { Box, ChakraProvider, extendTheme, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import '../styles/globals.css';
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+const theme = extendTheme({ colors });
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Flex flexDirection='row' p='2'>
+        <Box>
+          <Link href='/'>
+            <a className='pr-2'>Home</a>
+          </Link>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </Box>
+      </Flex>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
+import { Box } from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className=''>
+    <Box className='' color='red.600' fontWeight='bold'>
       <Head>
         <title>My Blog</title>
         <meta name='Nextjs Blog' content='This is Blog created with nextjs' />
@@ -11,9 +12,6 @@ export default function Home() {
       <main>
         <h1>My Blog</h1>
       </main>
-      <footer className='text-2xl text-red-800 text-center'>
-        Powered by M.Daud
-      </footer>
-    </div>
+    </Box>
   );
 }
