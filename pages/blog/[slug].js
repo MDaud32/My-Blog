33f4132542944e52e6bd7c4286b6f60a26f46fe8
MDaud32 <theprofessor543@gmail.com>
2022-01-17 13:15:40 +0500

    making dynamic pages responsive

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,20 +1,43 @@
+import { Box, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 import { blogPosts } from '../../lib/data';
 
 export default function BlogPost({ title, date, content }) {
   return (
-    <div className=''>
+    <div className='w-8/12 text-center mx-auto '>
       <Head>
         <title>posts</title>
         <meta name='About' content='This is Blog created with nextjs' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <h1>My Blog Posts</h1>
-        <h1>{title}</h1>
-        <p>{date.toString()}</p>
-        <p>{content}</p>
-      </main>
+
+      <Box
+        textAlign={'center'}
+        border={'2px'}
+        borderColor={'blackAlpha.400'}
+        my='6'
+        borderRadius={'3xl'}
+        h={{ base: 'fit-content' }}
+        px='4'
+        py={'4'}
+        shadow={'md'}
+      >
+        <Text
+          as='h1'
+          textColor={'gray.600'}
+          fontSize='xl'
+          fontWeight={'semibold'}
+          pb='3'
+        >
+          {title}
+        </Text>
+        <Text as='p' textColor={'gray.500'}>
+          {date}
+        </Text>
+        <Text as='p' textColor={'gray.600'} pt='3'>
+          {content}
+        </Text>
+      </Box>
     </div>
   );
 }
